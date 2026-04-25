e2ce0c1965a5c5039cbdfac4f4cc4b8851175d00
hrasity <hrasity@gmail.com>
2026-04-25 07:52:01 +0300

    ssh connection test

--- file/read_write.zig
+++ file/read_write.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
 
+    //Probably will change here! init.gpa
     const allocator = std.heap.page_allocator;
     const mem = try allocator.alloc(u8, 1024);
     defer allocator.free(mem);
